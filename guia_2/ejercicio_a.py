999457c01f9882c6e59b0f101014445ef6eaca57
Matías Fonseca <89324400+Tryton22@users.noreply.github.com>
2024-04-11 01:55:57 -0400

    Update ejercicio_a.py

--- guia_2/ejercicio_a.py
+++ guia_2/ejercicio_a.py
@@ -1,1 +1,18 @@
-print("Hola mundo")
+""" Ejercicio 1: Rotar circularmente las variables a, b, c y d de tipo int. """
+
+a = 43
+b = 12
+c = 34
+d = 70
+aux_1 = 0
+aux_2 = 0
+
+aux_1 = a
+a = d
+d = aux_1
+
+aux_2 = b
+b = c
+c = aux_2
+
+print("\n",a, b, c, d,"\n")
