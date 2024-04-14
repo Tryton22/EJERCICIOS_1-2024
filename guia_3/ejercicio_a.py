3fcfdca1b55b74689699c3111412610c882c036e
Matías Fonseca <89324400+Tryton22@users.noreply.github.com>
2024-04-13 22:20:45 -0400

    Update ejercicio_a.py

--- guia_3/ejercicio_a.py
+++ guia_3/ejercicio_a.py
@@ -1,1 +1,12 @@
-print("Hola mundo")
+""" Ejercicio 1:
+Hallar todos los numeros pares entre 10 y 100 incluyendo
+los extremos. """
+
+n = 1
+
+print("\nLos numeros pares entre 10 y 100 son: \n")
+
+for x in range(10,101):
+    if x % 2 == 0:
+        print("Numero par", n,":", x,"\n")
+        n += 1
