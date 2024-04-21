fe098f1b12c84fb9bd816bcbdc4596bf21846d77
Matías Fonseca <89324400+Tryton22@users.noreply.github.com>
2024-04-21 16:24:23 -0400

    Update ejercicio_t.py

--- guia_3/ejercicio_t.py
+++ guia_3/ejercicio_t.py
@@ -11,3 +11,17 @@ Alto: 5
 *        * 
 **********        """
 
+print("\nSolicitar un numero de 5 digitos por teclado y verificar si tiene 5 digitos de verdad.")
+print("------------------------------------------------------------------------------------------\n")
+bandera = 0
+
+while bandera < 2:
+    numero_5 = int(input("Ingrese un numero de 5 digitos: "))
+    if (numero_5 > 9999 and numero_5 < 100000) or (numero_5 < -9999 and numero_5 > -100000):
+        print("\nEl numero ingresado si tiene los 5 digitos y el numero es:", numero_5,"\n")
+        bandera = 2
+    elif bandera == 0:
+        print("\nDebe ingresar un numero de 5 digitos, intente nuevamente\n")
+        bandera = 1
+    else:
+        print("\nIntente nuevamente\n")
